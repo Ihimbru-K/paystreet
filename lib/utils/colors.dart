fdd0bcd85c3c55a82919b1a9f215dc9f2bdc8f12
Ihimbru-k <kanihims25@gmail.com>
2024-06-03 14:47:15 +0100

    created utils folder to contain ui colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,3 +5,4 @@ const Color balanceContainerColor = Colors.black;
 const Color buttonColor = Color(0xFF45466D);
 const Color transactionBackgroundColor = Color(0xFFEBEBFB);
 const Color whiteColor = Colors.white;
+//
